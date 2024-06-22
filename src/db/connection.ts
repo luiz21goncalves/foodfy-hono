afdac8f97eb7dcf095cd1559fd9a4d50d84f1929
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-22 14:16:13 -0300

    chore: add database connection

---
+++ src/db/connection.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from "pg";
+
+import { ENV } from "../env";
+
+const pool = new Pool({connectionString: ENV.DATABASE_URL})
+
+export const db = drizzle(pool)
