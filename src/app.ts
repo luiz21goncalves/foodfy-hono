f320febc104cbb9216bdbb54e58139d57eb0712e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-22 14:17:51 -0300

    feat: update status endpoint to show database status

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import { sql } from 'drizzle-orm'
 import { Hono } from 'hono'
 
 import { version } from '../package.json'
+import { db } from './db/connection'
+import { ENV } from './env'
 
 const app = new Hono()
 
@@ -8,10 +11,23 @@ app.get('/', (c) => {
   return c.json({message:'Hello Hono!'})
 })
 
-app.get('/v1/status', (c) => {
+app.get('/v1/status',async  (c) => {
+  const databaseVersion = (await db.execute<{server_version: string}>(sql`SHOW server_version`)).rows[0].server_version
+
+  const databaseMaxConnections = (await db.execute<{max_connections: string}>(sql`SHOW max_connections`)).rows[0].max_connections
+
+  const databaseOpenedConnections = (await db.execute<{count: number}>(sql`SELECT count(*)::int FROM pg_stat_activity WHERE datname = ${ENV.POSTGRESQL_DATABASE}`)).rows[0].count
+
   return c.json({
     updated_at: new Date().toISOString(),
-    version
+    version,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: Number(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections
+      }
+    }
   })
 })
 
