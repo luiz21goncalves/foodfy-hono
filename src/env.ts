822bf5701aac2c9e7e3323144021400c4ddb8381
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-22 13:53:33 -0300

    chore: add postgres service

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  PORT: z.coerce.number()
+  PORT: z.coerce.number(),
+  POSTGRESQL_USERNAME: z.string(),
+  POSTGRESQL_PASSWORD: z.string(),
+  POSTGRESQL_DATABASE: z.string(),
+  POSTGRESQL_HOST: z.string(),
+  POSTGRESQL_PORT: z.coerce.number(),
+  DATABASE_URL: z.string().url(),
 })
 
 export const ENV = envSchema.parse(Bun.env)
