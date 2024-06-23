f7d8f92c34a3b84dd87b7aa49d94322be3cc25f5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-23 13:52:02 -0300

    chore: add user migration

---
+++ drizzle/0000_typical_shaman.sql
@@ -0,0 +1,16 @@
+DO $$ BEGIN
+ CREATE TYPE "public"."user_role" AS ENUM('admin', 'publisher');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"name" text NOT NULL,
+	"password" text NOT NULL,
+	"role" "user_role" NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
