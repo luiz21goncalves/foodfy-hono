aaba13f3ecbb0b14e1b53c3a915f9aaf766fc50e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-23 10:55:23 -0300

    chore: add a custom logger when server starts

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,10 @@ import { ENV } from "@/env";
 
 import { app } from "./app";
 
-export default {
+const server = Bun.serve({
   fetch: app.fetch,
   port: ENV.PORT,
-};
+  hostname: "0.0.0.0",
+});
+
+console.log(`HTTP server running at: ${server.url}`);
