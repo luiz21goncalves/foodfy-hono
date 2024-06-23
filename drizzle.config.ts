f856eab26ffd7d3c8d1ec846b5f5194815577160
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-23 13:50:06 -0300

    chore: add drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { ENV } from "@/env";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dbCredentials: {
+    url: ENV.DATABASE_URL,
+  },
+  dialect: "postgresql",
+  out: "./drizzle",
+  schema: "./src/db/schema/index.ts",
+});
