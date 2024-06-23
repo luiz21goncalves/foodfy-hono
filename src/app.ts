e384ebc66a2df79ca5d1a549a08f2106c675564e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-23 09:16:20 -0300

    chore: add /api prefix

--- src/app.ts
+++ src/app.ts
@@ -39,4 +39,32 @@ app.get("/v1/status", async (c) => {
   });
 });
 
+app.get("/api/v1/status", async (c) => {
+  const databaseVersion = (
+    await db.execute<{ server_version: string }>(sql`SHOW server_version`)
+  ).rows[0].server_version;
+
+  const databaseMaxConnections = (
+    await db.execute<{ max_connections: string }>(sql`SHOW max_connections`)
+  ).rows[0].max_connections;
+
+  const databaseOpenedConnections = (
+    await db.execute<{ count: number }>(
+      sql`SELECT count(*)::int FROM pg_stat_activity WHERE datname = ${ENV.POSTGRESQL_DATABASE}`,
+    )
+  ).rows[0].count;
+
+  return c.json({
+    updated_at: new Date().toISOString(),
+    version,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: Number(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+  });
+});
+
 export { app };
