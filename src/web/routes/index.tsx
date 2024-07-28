330823150d32203680646430216fd04fd4731ca5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-07-28 10:11:25 -0300

    chore: add jsxRenderer middleware

--- src/web/routes/index.tsx
+++ src/web/routes/index.tsx
@@ -1,7 +1,16 @@
 import { Hono } from "hono";
+import { jsxRenderer } from "hono/jsx-renderer";
+
 import { Home } from "../pages/home";
 
 export const routes = new Hono();
+
+routes.get(
+  "*",
+  jsxRenderer(({ children }) => {
+    return <>{children}</>;
+  }),
+);
 
 routes.get("/", (c) => {
   return c.render(<Home />);
