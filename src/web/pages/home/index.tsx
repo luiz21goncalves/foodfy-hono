4179575a8b119cc6e687b6addc8aa8aa42b9e11b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-07-28 10:23:25 -0300

    chore: delete root-layout component

--- src/web/pages/home/index.tsx
+++ src/web/pages/home/index.tsx
@@ -1,9 +1,3 @@
-import { RootLayout } from "@/web/layouts/root-layout";
-
 export function Home() {
-  return (
-    <RootLayout>
-      <h1>As melhores receitas</h1>
-    </RootLayout>
-  );
+  return <h1>As melhores receitas</h1>;
 }
